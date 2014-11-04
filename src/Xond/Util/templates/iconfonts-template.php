e759c308b4bebeb75bbf72a35971f979dec857d7
Abah Zuckachev@MBPR <zaunaf@yahoo.com>
2014-11-05 05:19:43 +0700

    Only updated font urls in menu, and add closure to one issue comitted earlier: resolves #52

--- src/Xond/Util/templates/iconfonts-template.php
+++ src/Xond/Util/templates/iconfonts-template.php
@@ -8,15 +8,15 @@ Halo {{nama_pic}}. Ini adalah daftar icon yang bisa dilihat.
 <br><br>
 Font yang dimaksud di antaranya:
 <ul>
-<li><a href="/resources/font/font-computer-and-media/flaticon.html">font-computer-and-media</a></li>
-<li><a href="/resources/font/font-education/flaticon.html">font-education</a></li>
-<li><a href="/resources/font/font-fileformats-icons/flaticon.html">font-fileformats-icons</a></li>
-<li><a href="/resources/font/font-icomoon/flaticon.html">font-icomoon</a></li>
-<li><a href="/resources/font/FontAwesome/fontlist.html">FontAwesome</a></li>
-<li><a href="/resources/font/Ionicons/cheatsheet.html">Ionicons</a></li>
-<li><a href="/resources/font/Linecons/index.html">Linecons</a></li>
+<li><a href="/resources/fonts/font-computer-and-media/flaticon.html">font-computer-and-media</a></li>
+<li><a href="/resources/fonts/font-education/flaticon.html">font-education</a></li>
+<li><a href="/resources/fonts/font-fileformats-icons/flaticon.html">font-fileformats-icons</a></li>
+<li><a href="/resources/fonts/font-icomoon/flaticon.html">font-icomoon</a></li>
+<li><a href="/resources/fonts/FontAwesome/fontlist.html">FontAwesome</a></li>
+<li><a href="/resources/fonts/Ionicons/cheatsheet.html">Ionicons</a></li>
+<li><a href="/resources/fonts/Linecons/index.html">Linecons</a></li>
 <!--li><a href="#">montserrat</a></li-->
-<li><a href="/resources/font/pictos/index.html">pictos</a></li>
+<li><a href="/resources/fonts/pictos/index.html">pictos</a></li>
 <!--li><a href="#">raleway</a></li></ul-->
 </body>
 </html>
